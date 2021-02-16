c6460ef11b281548e779396493170784f752ebbc
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-02-16 11:54:11 +0100

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import entity.Student;
+import util.HibernateUtil;
+
+public class App {
+    public static void main(String[] args) {
+
+        Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
+        Student student1 = new Student("John", "Cena", "john@javaguides.com");
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(student);
+            session.save(student1);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List<Student> students = session.createQuery("from Student", Student.class).list();
+            students.forEach(s -> {
+                System.out.println("Print student email id : " + s.getEmail());
+            });
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+}
