44f134d5526aca304b905fa8729854bb2ee4188f
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-02-28 18:38:37 +0100

    Added foreign Key constraint to Consultation

--- src/main/java/domain/Consultation.java
+++ src/main/java/domain/Consultation.java
@@ -1,31 +1,22 @@
 package domain;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name="consultation")
 public class Consultation {
-
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="consultation_id")
     private int id;
 
+    @OneToOne
+    @JoinColumn(name="patient_id")
+    private Patient patient;
 
-    @OneToMany
-    private List<Patient> patients;
-    @JoinColumn(name="patient_id",referencedColumnName = "patients.patient_id")
-
-    private int patient_id;
-
-
-    @OneToMany
-    private List<Doctor> doctors;
-    @JoinColumn(name="doctor_id",referencedColumnName = "doctors.doctor_id")
-
-    private int doctor_id;
-
+    @OneToOne
+    @JoinColumn(name="doctor_id")
+    private Doctor doctor;
     @Column(name="date")
     private String date;
 
@@ -36,13 +27,29 @@ public class Consultation {
     @Column(name="time")
     private String time;
 
-    public Consultation(int id, int patient_id, int doctor_id, String date, int cabinetId, String time) {
+    public Consultation(int id, Patient patient, Doctor doctor, String date, int cabinetId, String time) {
         this.id = id;
-        this.patient_id = patient_id;
-        this.doctor_id = doctor_id;
+        this.patient = patient;
+        this.doctor = doctor;
         this.date = date;
         CabinetId = cabinetId;
         this.time = time;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
     }
 
     public int getId() {
@@ -51,22 +58,6 @@ public class Consultation {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public int getPatient_id() {
-        return patient_id;
-    }
-
-    public void setPatient_id(int patient_id) {
-        this.patient_id = patient_id;
-    }
-
-    public int getDoctor_id() {
-        return doctor_id;
-    }
-
-    public void setDoctor_id(int doctor_id) {
-        this.doctor_id = doctor_id;
     }
 
     public String getDate() {
