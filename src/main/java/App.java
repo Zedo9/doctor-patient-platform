5f962408c420ffad1371807463c995dcb757a328
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-02-28 18:36:03 +0100

    Cleaned App code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,46 +1,10 @@
-import java.util.List;
-
-import domain.Patient;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import domain.Student;
-import repo.PatientRepo;
-import repo.PatientRepoImpl;
-import util.HibernateUtil;
-
-
-
+import domain.Doctor;
+import repo.DoctorRepoImpl;
 
 public class App {
     public static void main(String[] args) {
-
-        Student student=new Student("mouwahed","mhadhbi","mouwahed66@gmail.com");
-       Transaction transaction=null;
-        try(Session session=HibernateUtil.getSessionFactory().openSession()){
-            transaction=session.beginTransaction();
-            session.save(student);
-            transaction.commit();
-        }catch (Exception e){
-            if(transaction!=null){
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-
-        try(Session sessions=HibernateUtil.getSessionFactory().openSession()) {
-            List<Student> students=sessions.createQuery("from Student",Student.class).list();
-            students.forEach(s->{
-                System.out.println("Student email: "+s.getEmail());
-            });
-        }
-        catch (Exception e){
-            if(transaction!=null){
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-
-
+        Doctor d1 = new Doctor("D1fname","D1lname","D1email","D1pass","D1spec");
+        DoctorRepoImpl repo = new DoctorRepoImpl();
+        repo.saveDoctor(d1);
     }
 }
