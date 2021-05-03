59c1e8fdee534cfbe212b0424631a660ec6e071e
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-05-03 04:22:03 +0100

    [FRONT]:Fixed root module routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DoctorDashboardComponent } from './doctor/doctor-dashboard/doctor-dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { PatientDashboardComponent } from './patient/patient-dashboard/patient-dashboard.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'doctor-dashboard', component: DoctorDashboardComponent },
-  { path: 'patient-dashboard', component: PatientDashboardComponent },
+  {
+    path: 'doctor-dashboard',
+    loadChildren: () =>
+      import(`./doctor/doctor.module`).then((m) => m.DoctorModule),
+  },
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,24 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { DoctorDashboardComponent } from './doctor/doctor-dashboard/doctor-dashboard.component';
-import { PatientDashboardComponent } from './patient/patient-dashboard/patient-dashboard.component';
-import { AppointmentCardComponent } from './doctor/appointment-card/appointment-card.component';
-import { PatientPageComponent } from './doctor/patient-page/patient-page.component';
-import { LayoutComponent } from './doctor/layout/layout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    DoctorDashboardComponent,
-    PatientDashboardComponent,
-    AppointmentCardComponent,
-    PatientPageComponent,
-    LayoutComponent,
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
