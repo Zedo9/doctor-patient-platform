f076b8d8c65e1cb9924abf9a0d5faa0ccabb6c78
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-05-11 03:53:08 +0100

    Added configuration for spec annotations

--- src/main/java/mygroup/appointments/AppointmentsApplication.java
+++ src/main/java/mygroup/appointments/AppointmentsApplication.java
@@ -1,12 +1,26 @@
 package mygroup.appointments;
 
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class AppointmentsApplication {
+@Configuration
+@EnableJpaRepositories
+public class AppointmentsApplication  implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentsApplication.class, args);
+	}
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(new SpecificationArgumentResolver());
 	}
 }
