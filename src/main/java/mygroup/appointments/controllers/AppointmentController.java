b883bcdccad56c5af18a7d3e10a1e57fce2a185d
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-05-22 09:56:59 +0100

    Prepared appointments controller

---
+++ src/main/java/mygroup/appointments/controllers/AppointmentController.java
@@ -0,0 +1,9 @@
+package mygroup.appointments.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/appointments")
+public class AppointmentController {
+}
